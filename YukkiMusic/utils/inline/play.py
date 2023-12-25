37bc7de922e139a3bf7c32875f02d55aaf1d6f5d
Xysz <115394912+Jrsss122@users.noreply.github.com>
2023-12-26 04:06:42 +0700

    .

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -17,7 +17,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_3"],  switch_inline_query_current_chat=""
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/JarsssStory",
+                text=_["S_B_9"], url=f"https://t.me/Chjarsss1",
             ),
         ],
         [
@@ -65,7 +65,7 @@ def stream_markup(_, videoid):
         [
      
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/JarsssStory",
+                text=_["S_B_9"], url=f"https://t.me/Chjarsss1",
             ),
 
 
@@ -88,7 +88,7 @@ def telegram_markup(_):
             ),
             InlineKeyboardButton(
 
-                text=_["S_B_9"], url=f"https://t.me/JarsssStory",
+                text=_["S_B_9"], url=f"https://t.me/Chjarsss1",
 
             ),
          ],
